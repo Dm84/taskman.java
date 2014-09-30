2277277375280e0132c4353ede83cead56e29763
dm84 <dmitriy.o84@ya.ru>
2014-09-30 21:55:45 +0600

    +дао задания

---
+++ src/main/java/com/test/dao/TaskDao.java
@@ -0,0 +1,30 @@
+package com.test.dao;
+
+import java.util.Map;
+
+import org.hibernate.SessionFactory;
+
+import com.test.domain.Task;
+
+
+class TaskDao implements ITaskDao {
+	
+	TaskDao() {
+		
+	}
+	
+	public void complete(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void create(Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public Map<Integer, Task> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
