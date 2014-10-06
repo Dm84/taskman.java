c6ebaaa1b89bcfbcfd7b8c1ea1a64d0a00938fb8
dm84 <dmitriy.o84@ya.ru>
2014-10-06 21:47:04 +0600

    heroku procfile

--- src/main/java/com/test/service/TaskService.java
+++ src/main/java/com/test/service/TaskService.java
@@ -9,25 +9,26 @@ import java.util.Map;
 
 @Service
 public class TaskService {
-	
+
 	private ITaskDao taskDao;
-	
+
 	public TaskService(ITaskDao taskDao) {
 		this.taskDao = taskDao;
 	}
-	
+
 	/**
-	 * Добавляет задание
+	 * Persists new task
+	 * 
 	 * @param task
 	 */
 	public int add(Task task) {
 		return taskDao.create(task);
 	}
-	
+
 	public void complete(Integer id) {
 		taskDao.complete(id);
 	}
-	
+
 	public Map<Integer, Task> listAll() {
 		return taskDao.listAll();
 	}
