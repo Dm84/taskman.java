3df8c56f2e04506f9728e7ffeb19203535d1743f
dm84 <dm84atgithub@ya.ru>
2021-12-31 15:59:36 +0500

    task service design changes

--- src/main/java/com/taskman/repository/TaskRepository.java
+++ src/main/java/com/taskman/repository/TaskRepository.java
@@ -24,38 +24,17 @@ class TaskRepository implements com.taskman.service.TaskRepository {
         return this.factory.getCurrentSession();
     }
 
-    /**
-     * Помечает задачу как завершенную
-     */
     public void complete(Integer id) {
-        Task task = (Task) getSession().byId(Task.class).getReference(id);
+        var task = getSession().byId(Task.class).getReference(id);
         task.setCompleted(true);
         getSession().update(task);
     }
 
-    /**
-     * Сохраняет новую задачу
-     */
     public Task create(Task task) {
         getSession().save(task);
         return task;
     }
 
-    /**
-     * Индексирует список задач
-     *
-     * @param taskCollection input collection
-     * @return task index
-     */
-    private Map<Integer, Task> IndexList(Collection<Task> taskCollection) {
-        return taskCollection.stream().collect(Collectors.toMap(Task::getId, task -> task));
-    }
-
-    /**
-     * Возвращает список всех задач
-     *
-     * @return get all tasks having
-     */
     public Collection<Task> findAll() {
         var builder = getSession().getCriteriaBuilder();
         var query = builder.createQuery(Task.class);
@@ -63,12 +42,6 @@ class TaskRepository implements com.taskman.service.TaskRepository {
         return getSession().createQuery(all).getResultList();
     }
 
-    /**
-     * Ищет задачи по части описания
-     *
-     * @param entry entry that description contains
-     * @return tasks matched string
-     */
     public Collection<Task> find(String entry) {
         var builder = getSession().getCriteriaBuilder();
         var query = builder.createQuery(Task.class);
--- src/main/java/com/taskman/service/TaskRepository.java
+++ src/main/java/com/taskman/service/TaskRepository.java
@@ -4,12 +4,29 @@ import java.util.Collection;
 
 public interface TaskRepository {
 
+    /**
+     * Помечает задачу как завершенную
+     */
     Task create(Task task);
 
+    /**
+     * Сохраняет новую задачу
+     */
     void complete(Integer id);
 
+    /**
+     * Возвращает список всех задач
+     *
+     * @return get all tasks having
+     */
     Collection<? extends Task> findAll();
 
-    Collection<? extends Task> find(String query);
+    /**
+     * Ищет задачи по части описания
+     *
+     * @param entry entry that description contains
+     * @return tasks matched string
+     */
+    Collection<? extends Task> find(String entry);
 }
 
